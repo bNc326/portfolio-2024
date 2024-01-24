72def9074fa37086331eb35735be554b8525daa6
bNc <kissbence326@gmail.com>
2024-01-24 14:26:46 +0100

    add source link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className='w-full p-2 text-center bg-[#333333] text-title text-h6 font-semibold'>{new Date().getFullYear().toString()}<span className='px-1'>&#xa9;</span>Kiss Bence</footer>
-  )
-}
+    <footer className="w-full p-2 bg-[#333333] flex flex-col text-title justify-center">
+      <div className="text-center text-h6 font-semibold">
+        {new Date().getFullYear().toString()}
+        <span className="px-1">&#xa9;</span>Kiss Bence
+      </div>
+      <a href="https://github.com/bNc326/portfolio-2024" target="_blank" rel="noreferrer" className="text-center">
+        source
+      </a>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
